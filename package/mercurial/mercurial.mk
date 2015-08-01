577c976d7b569f47d7f1a74cd0d518cdc8b12e21
noone@nowhere.net <noone@nowhere.net>
2015-08-01 13:01:56 -0700

    msg

--- package/mercurial/mercurial.mk
+++ package/mercurial/mercurial.mk
@@ -8,15 +8,15 @@ MERCURIAL_SOURCE = mercurial-$(MERCURIAL_VERSION).tar.gz
 MERCURIAL_SITE = http://mercurial.selenic.com/release
 MERCURIAL_LICENSE = GPLv2
 MERCURIAL_LICENSE_FILES = COPYING
-MERCURIAL_DEPENDENCIES = python bzip2 
+MERCURIAL_DEPENDENCIES = host-python 
 MERCURIAL_SETUP_TYPE = distutils
 define MERCURIAL_BUILD_CMDS
 	cd $(@D) &&\
-	$(TARGET_DIR)/usr/bin/python ./setup.py build 
+	$(HOST_DIR)/usr/bin/python ./setup.py build 
 endef
 define MERCURIAL_INSTALL_TARGET_CMDS
 	cd $(@D) &&\
-	$(TARGET_DIR)/usr/bin/python ./setup.py install --root="$(TARGET_DIR)" --prefix="/usr" --force
+	$(HOST_DIR)/usr/bin/python ./setup.py install --root="$(TARGET_DIR)" --prefix="/usr" --force
 endef
 #$(eval $(python-package))
 $(eval $(generic-package))
