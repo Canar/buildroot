67b341b6c29d121a95e516ac44e3f11d7fa1240a
Benjamin Cook <mail@benjamincook.ca>
2015-07-31 20:07:32 -0700

    msg

--- package/localedef/localedef.mk
+++ package/localedef/localedef.mk
@@ -8,6 +8,9 @@ LOCALEDEF_VERSION = 2.14.1-r17443-ptx1
 LOCALEDEF_SOURCE = localedef-eglibc-$(LOCALEDEF_VERSION).tar.bz2
 LOCALEDEF_SITE = http://www.pengutronix.de/software/ptxdist/temporary-src
 
+
+HOST_LOCALEDEF_CONF_ENV = CONFIG_SITE=no 
+
 HOST_LOCALEDEF_CONF_OPTS += \
 	--prefix=/usr \
 	--with-glibc=./eglibc
