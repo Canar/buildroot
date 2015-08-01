632fdf9e2ce5c6aba13aa01b51fcb9a2a8c7f334
me@me.me <me@me.me>
2015-07-31 22:31:48 -0700

    msg

--- package/readline/readline.mk
+++ package/readline/readline.mk
@@ -13,9 +13,21 @@ READLINE_CONF_ENV = bash_cv_func_sigsetjmp=yes \
 	bash_cv_wcwidth_broken=no
 READLINE_LICENSE = GPLv3+
 READLINE_LICENSE_FILES = COPYING
-READLINE_CONF_OPTS = \
-	--enable-multibyte \
-	--with-curses
+#READLINE_CONF_OPTS = \
+#	--enable-multibyte \
+#	--with-curses
+
+ifneq ($(BR2_PACKAGE_NCURSES_WCHAR),y)
+define READLINE_WIDE_CURSES
+cp $(@D)/configure $(@D)/configure.old
+sed 's/curses/cursesw/g' $(@D)/configure.old >$(@D)/configure
+#cp $(@D)/shlib/Makefile $(@D)/shlib/Makefile.old
+#sed 's/curses/cursesw/g' $(@D)/shlib/Makefile.old >$(@D)/shlib/Makefile
+cp $(@D)/support/shobj-conf $(@D)/support/shobj-conf.old
+sed 's/-lcurses/-lcursesw/g' $(@D)/support/shobj-conf.old >$(@D)/support/shobj-conf
+endef
+READLINE_POST_EXTRACT_HOOKS += READLINE_WIDE_CURSES
+endif
 
 define READLINE_PURGE_EXAMPLES
 	rm -rf $(TARGET_DIR)/usr/share/readline
