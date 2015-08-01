01d4eb8632d5e4c266dd27c36c0e2580031b6e98
me@me.me <me@me.me>
2015-08-01 01:59:37 -0700

    msg

--- package/readline/readline.mk
+++ package/readline/readline.mk
@@ -17,16 +17,22 @@ READLINE_LICENSE_FILES = COPYING
 #	--enable-multibyte \
 #	--with-curses
 
-ifneq ($(BR2_PACKAGE_NCURSES_WCHAR),y)
-define READLINE_WIDE_CURSES
-cp $(@D)/configure $(@D)/configure.old
-sed 's/curses/cursesw/g' $(@D)/configure.old >$(@D)/configure
-#cp $(@D)/shlib/Makefile $(@D)/shlib/Makefile.old
-#sed 's/curses/cursesw/g' $(@D)/shlib/Makefile.old >$(@D)/shlib/Makefile
-cp $(@D)/support/shobj-conf $(@D)/support/shobj-conf.old
-sed 's/-lcurses/-lcursesw/g' $(@D)/support/shobj-conf.old >$(@D)/support/shobj-conf
+ifeq ($(BR2_PACKAGE_NCURSES_WCHAR),y)
+define READLINE_EXT_WIDE_CURSES
+	cp $(@D)/configure $(@D)/configure.old
+	sed 's/-lncurses/-lncursesw/g' $(@D)/configure.old >$(@D)/configure
+	cp $(@D)/support/shobj-conf $(@D)/support/shobj-conf.old
+	sed 's/-lncurses/-lncursesw/g' $(@D)/support/shobj-conf.old >$(@D)/support/shobj-conf
 endef
-READLINE_POST_EXTRACT_HOOKS += READLINE_WIDE_CURSES
+READLINE_POST_EXTRACT_HOOKS += READLINE_EXT_WIDE_CURSES
+endif
+
+ifeq ($(BR2_PACKAGE_NCURSES_WCHAR),y)
+define READLINE_CONF_WIDE_CURSES
+	cp $(@D)/shlib/Makefile $(@D)/shlib/Makefile.old
+	sed 's/-lncurses/-lncursesw/g' $(@D)/shlib/Makefile.old >$(@D)/shlib/Makefile
+endef
+READLINE_POST_CONFIGURE_HOOKS += READLINE_CONF_WIDE_CURSES
 endif
 
 define READLINE_PURGE_EXAMPLES
